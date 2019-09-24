873666447d2f94d0af5b0d2ccadf37ce57647eb9
Leandro Ocampo <leandro.sys89@gmail.com>
2019-09-24 17:28:33 -0500

    new router

--- movies-api/routes/movies.js
+++ movies-api/routes/movies.js
@@ -17,6 +17,58 @@ function moviesApi(app) {
             next(err);
         }
     });
+
+    router.get('/:movieId', async (req, res, next) => {
+        try {
+            const movies = await Promise.resolve(moviesMock[0]);
+
+            res.status(200).json({
+                data: movies,
+                message: 'movie retrieved'
+            });
+        } catch (err) {
+            next(err);
+        }
+    });
+
+    router.post('/', async (req, res, next) => {
+        try {
+            const createdMovieID = await Promise.resolve(moviesMock[0].id);
+
+            res.status(201).json({
+                data: createdMovieID,
+                message: 'movie created'
+            });
+        } catch (err) {
+            next(err);
+        }
+    });
+
+    router.put('/:movieId', async (req, res, next) => {
+        try {
+            const updateMovieId = await Promise.resolve(moviesMock[0].id);
+
+            res.status(200).json({
+                data: updateMovieId,
+                message: 'movie updated'
+            });
+        } catch (err) {
+            next(err);
+        }
+    });
+
+    router.delete('/:movieId', async (req, res, next) => {
+        try {
+            const deleteMovieId = await Promise.resolve(moviesMock[0].id);
+
+            res.status(200).json({
+                data: deleteMovieId,
+                message: 'movie deleted'
+            });
+        } catch (err) {
+            next(err);
+        }
+    });
 }
 
 module.exports = moviesApi;
